81a86c96e1fcb6c7fcd62c56664cef4b7a92e826
Mehedi Hasan <mehedihasana383@gamil.com>
2025-04-05 01:14:42 +0600

    update authentication

--- Backend/middleware/authentication.js
+++ Backend/middleware/authentication.js
@@ -3,18 +3,41 @@ import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import jwt from 'jsonwebtoken';
 
-const isAuthenticatedUser=asyncHandler(async(req,res,next)=>{
-    const {token}=req.cookies;
+// const isAuthenticatedUser=asyncHandler(async(req,res,next)=>{
+//     const {token}=req.cookies;
 
-    if(!token)
-        return next(new ApiError(401,'Please login to access this resource'));
+//     if(!token)
+//         return next(new ApiError(401,'Please login to access this resource'));
 
-    const decodedToken=jwt.verify(token,process.env.JWT_SECRET);
+//     const decodedToken=jwt.verify(token,process.env.JWT_SECRET);
 
-    const user=await User.findById(decodedToken.id);
+//     const user=await User.findById(decodedToken.id);
 
-    req.user=user;
-    next();
+//     req.user=user;
+//     next();
+// });
+
+const isAuthenticatedUser = asyncHandler(async (req, res, next) => {
+    // Get token from cookies or Authorization header
+    const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
+    
+    if (!token) {
+        return next(new ApiError(401, 'Please login to access this resource'));
+    }
+
+    try {
+        const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+        const user = await User.findById(decodedToken.id);
+        
+        if (!user) {
+            return next(new ApiError(401, 'User not found'));
+        }
+
+        req.user = user;
+        next();
+    } catch (error) {
+        return next(new ApiError(401, 'Invalid or expired token'));
+    }
 });
 
 const isRoleAdmin=(role)=>{
